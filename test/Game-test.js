2068453cd4aa1f2053cd3fc4aa0be0d13b551f1a
Carly Clift <carlymclift@gmail.com>
2020-07-01 21:59:45 -0600

    Start testing file for the Game class and the star() method of the Game class

---
+++ test/Game-test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Game = require('../src/Game');
+
+describe('Game', () => {
+
+  let game;
+    
+  beforeEach(() => {
+    game = new Game()
+  })
+
+  it('Should be a function', () => {
+    expect(Game).to.be.a('function');
+  })
+
+  it('Should be an instance of Deck', () => {
+    expect(game).to.be.an.instanceOf(Game)
+  })
+
+  it('Should keep track of the current round', () => {
+    expect(game.currentRound).to.equal(undefined);
+  })
+
+  it('should start the game with a new round', () => {
+    game.start()
+    expect(game.currentRound).to.be.an.instanceOf(Round);
+  })
+
+  it('should start the game with a new set of cards', () => {
+    game.start()
+    expect(game.currentRound.deck[0]).to.be.an.instanceOf(Card);
+  })
+})
